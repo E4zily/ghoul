47c4b2e664ab6b7c0d92edba6f8df96e8e876d95
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 17:17:46 +0300

    Create ghoul.rs

---
+++ ghoul.rs
@@ -0,0 +1,11 @@
+use std::{thread, time::Duration};
+
+fn main() {
+
+	let mut h = 1000;
+	println!("{}", h);
+	thread::sleep(Duration::from_millis(1000));
+
+	while h > -1 { h = h - 7; println!("{}", h); thread::sleep(Duration::from_millis(25)); }
+
+}
