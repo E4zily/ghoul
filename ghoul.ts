d8c8262479a30f09f184f806c07fe746c793bf3a
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 20:12:41 +0300

    Create ghoul.ts

---
+++ ghoul.ts
@@ -0,0 +1,13 @@
+let h = 1000
+
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+console.log(1000)
+delay(2000)
+while (h > -1) {
+    h = h - 7
+    console.log(h)
+    delay(50)
+}
