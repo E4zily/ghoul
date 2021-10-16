a425ba337a80fe02e6224084f7fd8f0151689813
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 17:59:37 +0300

    Create ghoul.java

---
+++ ghoul.java
@@ -0,0 +1,28 @@
+package com.plutoned1.ghoul;
+
+import java.util.concurrent.TimeUnit;
+
+public class main {
+
+	public static void main(String[] args) {
+		int h = 1000;
+		System.out.println(h);
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		while (h > -1)
+		{
+			h = h - 7;
+			System.out.println(h);
+			
+			try {
+				TimeUnit.MILLISECONDS.sleep(25);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
