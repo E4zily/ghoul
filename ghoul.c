bc297a4df1f2b775644e53dd60109fa96708c2f4
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 19:22:48 +0300

    Create ghoul.c

---
+++ ghoul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+int main() {
+	int h = 1000;
+	printf("\n1000");
+	sleep(3);
+	
+	while(h > -1) {
+		h = h - 7;
+		char str[5];
+		sprintf(str, "%d", h);
+	    printf("\n%s", str);
+		usleep(10000);
+	}
+	printf("\n ");
+    return 0;
+}
+  
