5d6266aa566b8d45609ba2ee47c37e59890ba972
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 18:43:57 +0300

    Create ghoul.go

---
+++ ghoul.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+  var i int32 = 1000
+  fmt.Println(i)
+  time.Sleep(time.Second)
+
+  for i > -1 {
+    i = i-7
+    fmt.Println(i)
+    time.Sleep(time.Second/45)
+  }
+}
