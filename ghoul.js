25c86a8c4a4d741284770f8fbb8eca605f636e6a
E4zily <59582293+E4zily@users.noreply.github.com>
2021-10-16 16:34:40 +0300

    Create ghoul.js

---
+++ ghoul.js
@@ -0,0 +1,18 @@
+h = 1000
+async function init() {
+	console.log(h)
+	await sleep(1000)
+	while (h > -1) {
+		h = h - 7
+		console.log(h)
+		await sleep(25);
+	}
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+init()
